e72d1a1b8e9fc1e07cb3fbca6f889f5ef396d055
sararejimand <sararezhimand@gmail.com>
2025-09-26 10:33:37 +0200

    add audio url to database and add music player

--- app/Models/Songs.php
+++ app/Models/Songs.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-use App\Models\Singers;
+use App\Models\Singers; 
 use Illuminate\Database\Eloquent\Model;
 
 class Songs extends Model
--- resources/views/Songs/info.blade.php
+++ resources/views/Songs/info.blade.php
@@ -1,11 +1,34 @@
 <a href="{{ route('songs.list') }}">Back</a>
 
-
 <div>
-    <h1>{{$song->singer}}</h1>
-    <h2>{{$song->title}}</h2>
-    <p>Category: {{$song->category}}</p>
-    <img src="{{$song->photo}}" alt="">
-    <iframe src="{{$song->audio}}" frameborder="0"></iframe>
-    <iframe src="{{$song->lyrics}}" frameborder="0"></iframe>
+    <h1>{{ $song->singer }}</h1>
+    <h2>{{ $song->title }}</h2>
+
+    <p>{{ $song->singer }}</p>
+
+            @if($song->audio)
+                @php
+                    $raw = $song->audio;
+                    $audioSrc = preg_match('/^https?:\\/\\//i', $raw) ? $raw : asset('storage/' . ltrim($raw, '/'));
+                @endphp
+
+                <audio id="song-audio" controls preload="none">
+                    <source id="song-source" src="{{ $audioSrc }}" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+            @else
+                <p>No audio available.</p>
+            @endif
+
+            @if($song->photo)
+                <img src="{{ $song->photo }}" alt="">
+            @endif
+
+            @if($song->lyrics && preg_match('/^https?:\\/\\//i', $song->lyrics))
+                <iframe src="{{ $song->lyrics }}" frameborder="0"></iframe>
+            @elseif($song->lyrics)
+                <div class="lyrics">
+                    {!! nl2br(e($song->lyrics)) !!}
+                </div>
+            @endif
 </div>
